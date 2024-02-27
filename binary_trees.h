a8dd1caaa352bc020891a9a2293b0215afdfe0d1
garie-lhilarion <gabrielhilarion@gmail.com>
2024-02-27 15:56:07 +0100

    Make correction header file

--- binary_trees.h
+++ binary_trees.h
@@ -34,6 +34,6 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 
 void binary_tree_print(const binary_tree_t *tree);
 
-int binary_tree_is_root(const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node);
 
 #endif /* BINARY_TREES_H */
